05d78d5b01b6589223a2e93a95a775296e09a09a
shahar-Y <shya95@gmail.com>
2019-07-04 13:37:04 +0300

    added grpc error verification #87
    Signed-off-by: shahar-Y <shya95@gmail.com>

--- src/file/file.rpc.ts
+++ src/file/file.rpc.ts
@@ -6,7 +6,7 @@ import { GrpcHealthCheck, HealthCheckResponse, HealthService } from 'grpc-ts-hea
 import { FileService } from './file.service';
 import { IFile } from './file.interface';
 import { elasticURL } from '../config';
-import { statusToString } from '../utils/errors/grpc.status';
+import { statusToString, validateGrpcError } from '../utils/errors/grpc.status';
 
 apm.start({
   serviceName: 'file-service',
@@ -37,8 +37,6 @@ export const healthCheckStatusMap = {
   serviceName: HealthCheckResponse.ServingStatus.UNKNOWN
 };
 
-
-
 /**
  * The FileServer class, containing all of the FileServer methods.
  */
@@ -59,33 +57,35 @@ export class FileServer {
     this.server.addService(HealthService, this.grpcHealthCheck);
 
     const fileService = {
-      GenerateKey: this.wrapper(this.generateKey),
-      CreateUpload: this.wrapper(this.createUpload),
-      UpdateUploadID: this.wrapper(this.updateUpload),
-      GetUploadByID: this.wrapper(this.getUploadByID),
-      DeleteUploadByID: this.wrapper(this.deleteUploadByID),
-      GetFileByID: this.wrapper(this.getFileByID),
-      GetFileByKey: this.wrapper(this.getFileByKey),
-      GetFilesByFolder: this.wrapper(this.getFilesByFolder),
-      CreateFile: this.wrapper(this.createFile),
-      DeleteFile: this.wrapper(this.deleteFile),
-      IsAllowed: this.wrapper(this.isAllowed),
+      GenerateKey: this.wrapper(this.GenerateKey),
+      CreateUpload: this.wrapper(this.CreateUpload),
+      UpdateUploadID: this.wrapper(this.UpdateUpload),
+      GetUploadByID: this.wrapper(this.GetUploadByID),
+      DeleteUploadByID: this.wrapper(this.DeleteUploadByID),
+      GetFileByID: this.wrapper(this.GetFileByID),
+      GetFileByKey: this.wrapper(this.GetFileByKey),
+      GetFilesByFolder: this.wrapper(this.GetFilesByFolder),
+      CreateFile: this.wrapper(this.CreateFile),
+      DeleteFile: this.wrapper(this.DeleteFile),
+      IsAllowed: this.wrapper(this.IsAllowed),
     };
 
     this.server.addService(file_proto.FileService.service, fileService);
   }
 
-  private wrapper (func: Function) : any {
+  private wrapper (func: Function) :
+  (call: grpc.ServerUnaryCall<Object>, callback: grpc.requestCallback<Object>) => Promise<void> {
     return async (call: grpc.ServerUnaryCall<Object>, callback: grpc.requestCallback<Object>) => {
       try {
         const traceparent = call.metadata.get('elastic-apm-traceparent');
         const transOptions = (traceparent.length > 0) ? { childOf: traceparent[0].toString() } : {};
         apm.startTransaction(`/file.FileService/${func.name}`, 'request', transOptions);
         const res = await func(call, callback);
-        apm.endTransaction('OK');
+        apm.endTransaction(statusToString(grpc.status.OK));
         callback(null, res);
       } catch (err) {
-        const errName = err.code ? statusToString(err.code) : 'UNKNOWN';
+        const validatedErr = validateGrpcError(err);
+        const errName = statusToString(validatedErr.code);
         apm.endTransaction(errName);
         callback(err);
       }
@@ -96,12 +96,12 @@ export class FileServer {
   // ******************** UPLOAD FUNCTIONS ******************** */
 
   // Generates a random key for the upload.
-  private generateKey(call: any, callback: any) {
+  private GenerateKey(call: any, callback: any) {
     return { key: FileService.generateKey() };
   }
 
   // Creates an upload object, present while uploading a file.
-  private async createUpload(call: any, callback: any) {
+  private async CreateUpload(call: any, callback: any) {
     const key: string = FileService.generateKey();
     const bucket: string = call.request.bucket;
     const name: string = call.request.name;
@@ -112,7 +112,7 @@ export class FileServer {
   }
 
   // Updates the uploadID.
-  private async updateUpload(call: any, callback: any) {
+  private async UpdateUpload(call: any, callback: any) {
     const key: string = call.request.key;
     const uploadID: string = call.request.uploadID;
     const bucket: string = call.request.bucket;
@@ -120,13 +120,13 @@ export class FileServer {
   }
 
   // Get an upload metadata by its id in the DB.
-  private async getUploadByID(call: any, callback: any) {
+  private async GetUploadByID(call: any, callback: any) {
     const id = call.request.uploadID;
     return FileService.getUploadById(id);
   }
 
   //  Delete an upload from the DB by its id.
-  private async deleteUploadByID(call: any, callback: any) {
+  private async DeleteUploadByID(call: any, callback: any) {
     const id = call.request.uploadID;
     return FileService.deleteUpload(id);
   }
@@ -134,7 +134,7 @@ export class FileServer {
   // ********************* FILE FUNCTIONS ********************* */
 
   // Creates a new file in the DB.
-  private async createFile(call: any, callback: any) {
+  private async CreateFile(call: any, callback: any) {
     const params = call.request;
     const createdFile = await FileService.create(
       params.bucket,
@@ -149,28 +149,28 @@ export class FileServer {
   }
 
   // Deletes a file, according to the file deletion policy.
-  private async deleteFile(call: any, callback: any) {
+  private async DeleteFile(call: any, callback: any) {
     const id: string = call.request.id;
     await FileService.delete(id);
     return { ok: true };
   }
 
   // Retrieves a file by its id.
-  private async getFileByID(call: any, callback: any) {
+  private async GetFileByID(call: any, callback: any) {
     const id: string = call.request.id;
     const file = await FileService.getById(id);
     return new ResFile(file);
   }
 
   // Retrieves a file by its key.
-  private async getFileByKey(call: any, callback: any) {
+  private async GetFileByKey(call: any, callback: any) {
     const key: string = call.request.key;
     const file = await FileService.getByKey(key);
     return new ResFile(file);
   }
 
   // Retrieves all files residing in a given folder.
-  private async getFilesByFolder(call: any, callback: any) {
+  private async GetFilesByFolder(call: any, callback: any) {
     const folderID: string = call.request.folderID;
     const ownerID: string = call.request.ownerID;
     const files = await FileService.getFilesByFolder(folderID, ownerID);
@@ -180,7 +180,7 @@ export class FileServer {
   }
 
   // Checks if an operation is allowed by permission of the owner.
-  private async isAllowed(call: any, callback: any) {
+  private async IsAllowed(call: any, callback: any) {
     const res = await FileService.isOwner(call.request.fileID, call.request.userID);
     return  { allowed: res };
   }
