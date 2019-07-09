43ea4ec2319724c2cb2c263a46464741be8333f4
shahar-Y <shya95@gmail.com>
2019-07-09 10:53:28 +0300

    changed server file to function #93 #94
    Signed-off-by: shahar-Y <shya95@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,56 +1,41 @@
 import mongoose from 'mongoose';
-import { mongoConnectionString, rpcPort } from './config';
+import { mongoConnectionString, rpcPort, nodeEnv } from './config';
 import { FileServer, serviceNames } from './file/file.rpc';
 import { HealthCheckResponse } from 'grpc-ts-health-check';
 
-export class Server {
-  public listener: any;
-
-  constructor(testing = false) {
-    if (!testing) {
-      this.connectDB();
-    }
-  }
-
-  public static bootstrap(): Server {
-    return new Server();
-  }
-
-  // Connect mongoose to our database
-  private async connectDB() {
-    const fileServer: FileServer = new FileServer(rpcPort);
-
-    await mongoose.connect(
-      mongoConnectionString,
-      { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false },
-      (err) => {
-        if (!err) {
-          setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.SERVING);
-        } else {
-          setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.NOT_SERVING);
-        }
-      });
-
-    const db = mongoose.connection;
-    db.on('connected', () => {
-      setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.SERVING);
-    });
-    db.on('error', () => {
-      setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.NOT_SERVING);
-    });
-    db.on('disconnected', () => {
-      setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.NOT_SERVING);
-    });
-
-    // Ensures you don't run the server twice
-    if (!module.parent) {
-      fileServer.server.start();
-    }
-  }
+async function initServer() {
+  const fileServer: FileServer = new FileServer(rpcPort);
+  await connectDB(fileServer);
+  fileServer.server.start();
 }
 
+async function connectDB(fileServer: FileServer) {
+  await mongoose.connect(
+    mongoConnectionString,
+    { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false },
+    (err) => {
+      if (!err) {
+        setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.SERVING);
+      } else {
+        setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.NOT_SERVING);
+      }
+    });
+
+  const db = mongoose.connection;
+  db.on('connected', () => {
+    setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.SERVING);
+  });
+  db.on('error', () => {
+    setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.NOT_SERVING);
+  });
+  db.on('disconnected', () => {
+    setHealthStatus(fileServer, HealthCheckResponse.ServingStatus.NOT_SERVING);
+  });
+}
+
+// Ensures you don't run the server twice
 if (!module.parent) {
-  new Server();
+  initServer();
 }
 
 function setHealthStatus(server: FileServer, status: number) : void {
