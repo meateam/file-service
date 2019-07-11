03148b71b99d912679baa98b4ce52af6ccb8c98b
shahar-Y <shya95@gmail.com>
2019-07-11 13:07:31 +0300

    fixed grpc status codes
    Signed-off-by: shahar-Y <shya95@gmail.com>

--- src/utils/errors/client.error.ts
+++ src/utils/errors/client.error.ts
@@ -1,43 +1,44 @@
 import { ClientError } from './application.error';
+import * as grpc from 'grpc';
 
 export class IdInvalidError extends ClientError {
   constructor(message?: string) {
-    super(message || 'invalid id', 3);
+    super(message || 'invalid id', grpc.status.INVALID_ARGUMENT);
   }
 }
 
 export class NameInvalidError extends ClientError {
   constructor(message?: string) {
-    super(message || 'invalid name', 3);
+    super(message || 'invalid name', grpc.status.INVALID_ARGUMENT);
   }
 }
 
 export class MailInvalidError extends ClientError {
   constructor(message?: string) {
-    super(message || 'invalid mail', 3);
+    super(message || 'invalid mail', grpc.status.INVALID_ARGUMENT);
   }
 }
 
 export class KeyAlreadyExistsError extends ClientError {
   constructor(key:string, message?: string) {
-    super(message || `key: ${key} is already in use`, 3);
+    super(message || `key: ${key} is already in use`, grpc.status.INVALID_ARGUMENT);
   }
 }
 
 export class FileExistsWithSameName extends ClientError {
   constructor(message?: string) {
-    super(message || 'file already exists in the folder', 3);
+    super(message || 'file already exists in the folder', grpc.status.INVALID_ARGUMENT);
   }
 }
 
 export class FileNotFoundError extends ClientError {
   constructor(message?: string) {
-    super(message || 'file not found', 5);
+    super(message || 'file not found', grpc.status.NOT_FOUND);
   }
 }
 
 export class UploadNotFoundError extends ClientError {
   constructor(message?: string) {
-    super(message || 'upload not found', 5);
+    super(message || 'upload not found', grpc.status.NOT_FOUND);
   }
 }
