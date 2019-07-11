8506c85c36687891926a7771fa2668ffd31732fa
shahar-Y <shya95@gmail.com>
2019-07-11 12:29:38 +0300

    added nyc, deleted unused code. everything woeks #86
    Signed-off-by: shahar-Y <shya95@gmail.com>

--- src/utils/spec.helper.ts
+++ src/utils/spec.helper.ts
@@ -1,9 +1,7 @@
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
-import * as chai from 'chai';
 import { database } from '../config';
 
-const expect: Chai.ExpectStatic = chai.expect;
 dotenv.config({ path: '.env' });
 
 (<any>mongoose).Promise = Promise;
@@ -12,32 +10,6 @@ process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
   // application specific logging, throwing an error, or other logic here
 });
-
-const mochaAsync = (func: Function) => {
-  return async (done: Function) => {
-    try {
-      await func();
-      done();
-    } catch (err) {
-      done(err);
-    }
-  };
-};
-
-export const expectError = async (func: Function, params: any[]) => {
-  let isError = false;
-  let currError;
-  try {
-    await func(...params);
-  } catch (err) {
-    currError = err;
-    err.should.exist;
-    isError = true;
-  } finally {
-    isError.should.be.true;
-    return currError;
-  }
-};
 
 before(async () => {
   const mongoHost = process.env.MONGO_HOST || database.host;
