4a16a8925e8de2a0f55a5143b4e4b82829820b88
Yosef Romano <yosef32@gmail.com>
2020-12-20 15:49:13 +0200

    test

--- src/file/file.service.spec.ts
+++ src/file/file.service.spec.ts
@@ -120,15 +120,7 @@ describe('File Logic', () => {
     });
 
     it('should throw an error if file with the same name, ownerID and parent exists', async () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-      await FileService.create(bucket, 'file.txt', USER.id, 'text', KEY, KEY2, size);
-=======
       await FileService.create(bucket, 'file.txt', USER.id, 'text', 'drive', KEY, KEY2), size;
->>>>>>> origin/develop
-=======
-      await FileService.create(bucket, 'file.txt', USER.id, 'text', KEY, KEY2, size);
->>>>>>> bb6494f7608a64dc510bf06beb06a7f3c1e29b20
       await UploadService.createUpload(KEY3, bucket, 'file.txt', USER.id, KEY)
         .should.eventually.be.rejectedWith(FileExistsWithSameName);
     });
@@ -170,17 +162,7 @@ describe('File Logic', () => {
   });
 
   describe('#createUpdate', () => {
-
-    it('should not throw an error when {ownerID, parent, name} already exist', async () => {
-      await FileService.create(bucket, 'file.txt', USER.id, 'text', null, KEY3, size);
-      const newUpdate1: IUpload =
-        await UploadService.createUpdate(KEY, bucket, 'file.txt', USER.id, null)
-          .should.eventually.exist;
-      const newUpdate2: IUpload =
-        await UploadService.createUpdate(KEY2, bucket, 'file.txt', USER.id, null)
-          .should.eventually.exist;
-    });
-
+    
     it('should return a new upload', async () => {
       await FileService.create(bucket, 'file.txt', USER.id, 'text', 'drive', KEY, KEY2, size);
       const newUpdate: IUpload = await UploadService.createUpdate(KEY, bucket, 'file.txt', USER.id, KEY, size).should.eventually.exist;
